2a52ccd41f5e8e0f361eae9714585a0cd5c59a9b
Wei Lun <weilun_95@hotmail.com>
2018-09-29 17:39:10 +0800

    added ingredient to recipe

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipes/recipe.model';
+import {Ingredient} from './shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
@@ -7,10 +8,26 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test recipe 1', 'test description 1', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
-    new Recipe('Test recipe 2', 'test description 2', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
-    new Recipe('Test recipe 3', 'test description 3', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg'),
-    new Recipe('Test recipe 4', 'test description 4', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg')
+    new Recipe(
+      'Test recipe 1',
+      'test description 1',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
+      [new Ingredient('Apple', 5)]),
+    new Recipe(
+      'Test recipe 2',
+      'test description 2',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
+      [new Ingredient('Orange', 6)]),
+    new Recipe(
+      'Test recipe 3',
+      'test description 3',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
+      [new Ingredient('Water', 10)]),
+    new Recipe(
+      'Test recipe 4',
+      'test description 4',
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_1280.jpg',
+      [new Ingredient('Kiwi', 12)])
   ];
 
   constructor() {
